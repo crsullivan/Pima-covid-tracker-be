080389a9e0f52ff94b3a8f73d442af40d7270a97
Connor Sullivan <crsullivan@protonmail.com>
2020-06-18 15:21:15 -0700

    auth-router fix

--- admin/auth-router.js
+++ admin/auth-router.js
@@ -21,29 +21,29 @@ router.post('/register', (req, res) => {
         res.status(500).json(error);
         });
     });
-  
-  router.post('/login', (req, res) => {
-    let { name, password } = req.body;
-    Admin.findBy({ name })
-      .then(user => {
-        console.log("User:", user)
-        console.log("Bcrypt stuff:", bcrypt.compareSync(password, user.password))
-        console.log("T/F:", user && bcrypt.compareSync(password, user.password))
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = getJwtToken(user.name, user.id);
-          console.log('compareSync Success')
-          res.status(200).json({
-            message: `Welcome ${user.name}!`,
-            token
-          });
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
+
+router.post('/login', (req, res) => {
+  let { name, password } = req.body;
+
+  Admin.findBy({ name })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.name, user.id);
+        console.log("USERID", user.id);
+        console.log("NAME", user.name)
+        res.status(200).json({
+          message: `Welcome ${user.name}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 function getJwtToken(name, userId) {
     const payload = {
