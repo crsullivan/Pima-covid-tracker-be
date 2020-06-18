93cfbbf50a3cfdca9cebc9b033f2455257807908
Connor Sullivan <crsullivan@protonmail.com>
2020-06-18 15:55:06 -0700

    table update

--- database/migrations/20200618132819_lauras-graph.js
+++ database/migrations/20200618132819_lauras-graph.js
@@ -4,13 +4,13 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('admin', t => {
         t.increments();
-        t.string('password', 25).notNullable();
+        t.text('password', 25).notNullable();
         t.text('name', 25).notNullable().unique();;
     })
 
     .createTable('data', t => {
         t.increments();
-        t.string('date', 5).notNullable();
+        t.text('date', 5).notNullable();
         t.integer('value', 5).notNullable();;
     })
 
