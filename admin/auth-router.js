9df722851b34f27a98c086add7836ca2fda8844d
Connor Sullivan <crsullivan@protonmail.com>
2020-06-18 16:11:24 -0700

    update to auth-router

--- admin/auth-router.js
+++ admin/auth-router.js
@@ -22,28 +22,22 @@ router.post('/register', (req, res) => {
         });
     });
 
-router.post('/login', (req, res) => {
-  let { name, password } = req.body;
-
-  Admin.findBy({ name })
-    .first()
-    .then(user => {
+router.post('/login', (req, res) =>{
+  let {name, password} = req.body
+  Admin.findBy({name})
+  .first()
+  .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = getJwtToken(user.name, user.id);
-        console.log("USERID", user.id);
-        console.log("NAME", user.name)
-        res.status(200).json({
-          message: `Welcome ${user.name}!`,
-          token
-        });
+          const token = generateToken(user)
+          res.status(200).json({message: `Welcome ${user.name}!`, token})
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+          res.status(401).json({message: "This is not a registered user"})
       }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+  })
+  .catch(error => {
+      res.status(500).json(error)
+  })
+})
 
 router.get('/lemmesee', (req, res) => {
   Admin.getAll()
