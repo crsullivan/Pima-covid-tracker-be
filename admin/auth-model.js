0b4ad5d659269ab150712dfba5807a3ca20bdf2b
Connor Sullivan <crsullivan@protonmail.com>
2020-06-18 20:26:06 -0700

    Limits access

--- admin/auth-model.js
+++ admin/auth-model.js
@@ -1,30 +1,30 @@
 const db = require('../database/dbconfig.js');
 
 module.exports = {
-    add,
+    // add,
     findBy,
     getAll,
     findById
 };
 
-async function add(user) {
-  if (process.env.DB_ENV === 'production') {
-    console.log(db.insert(user).into('admin').returning('id'), "add method")
-    return db.insert(user).into('admin').returning('id')
-    .then(ids=> {
-      console.log(ids[0]);
-      return findById(ids[0]);
-    })
-    .catch(error => {
-      console.log(error);
-      throw error;
-    });
-  } else {
-    console.log(user)
-    const [id] = await db('admin').insert(user)
-    return findById(id)
-  }
-}
+// async function add(user) {
+//   if (process.env.DB_ENV === 'production') {
+//     console.log(db.insert(user).into('admin').returning('id'), "add method")
+//     return db.insert(user).into('admin').returning('id')
+//     .then(ids=> {
+//       console.log(ids[0]);
+//       return findById(ids[0]);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//       throw error;
+//     });
+//   } else {
+//     console.log(user)
+//     const [id] = await db('admin').insert(user)
+//     return findById(id)
+//   }
+// }
 
 function findBy(name) {
   return db('admin').where(name);
--- admin/auth-router.js
+++ admin/auth-router.js
@@ -6,21 +6,21 @@ const bcrypt = require('bcryptjs');
 const Admin = require('../admin/auth-model.js')
 const jwt = require('jsonwebtoken')
 
-router.post('/register', (req, res) => {
+// router.post('/register', (req, res) => {
 
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-    user.password = hash;
+//     let user = req.body;
+//     const hash = bcrypt.hashSync(user.password, 10);
+//     user.password = hash;
 
-    Admin.add(user)
-        .then(user => {
-        res.status(201).json({ message: `Admin ${user.name} registered` });
-        console.log("Registered")
-        })
-        .catch(error => {
-        res.status(500).json(error);
-        });
-    });
+//     Admin.add(user)
+//         .then(user => {
+//         res.status(201).json({ message: `Admin ${user.name} registered` });
+//         console.log("Registered")
+//         })
+//         .catch(error => {
+//         res.status(500).json(error);
+//         });
+//     });
 
 router.post('/login', (req, res) => {
   let { name, password } = req.body;
@@ -29,7 +29,7 @@ router.post('/login', (req, res) => {
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user.name, user.id);
+          const token = generateToken(user.name);
           console.log("USERID", user.id);
           res.status(200).json({
             message: `Welcome ${user.name}!`,
@@ -54,10 +54,9 @@ router.get('/lemmesee', (req, res) => {
     });
 })
 
-function generateToken(name, id) {
+function generateToken(name) {
     const payload = {
-      name,
-      id
+      name
     }
     const options = {
       expiresIn: '4h'
